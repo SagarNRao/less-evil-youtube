854bb7755cfd1e30d9830171699a1a16246db74e
SagarNRao <sagarnrao154@gmail.com>
2025-01-19 23:59:45 +0530

    can take input from frontend and make predictions

--- server/server.py
+++ server/server.py
@@ -5,10 +5,12 @@ import sys
 app = Flask(__name__)
 CORS(app)
 
+input_data = None
 
 @app.route("/model", methods=["POST"])
 def review():
-    data = request.get_json()
+    global input_data
+    input_data = request.get_json()
     # return jsonify({"message": True})
     return jsonify({"message": False})
 
@@ -17,6 +19,15 @@ def review():
 
     # hardcoded for testing purposes will use an ML model
 
-
+@app.route("/get_input_data", methods=["GET"])
+def get_input_data():
+    global input_data
+    print("Data sent:", input_data)  # Debugging statement
+    if input_data is not None:
+        return jsonify(input_data)
+    else:
+        return jsonify({"message": "No data available"}), 404
+    
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- src/app/Player/page.tsx
+++ src/app/Player/page.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { useSearchParams } from "next/navigation";
 import ReactPlayer from "react-player";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 export default function Player() {
   const searchParams = useSearchParams();
@@ -11,10 +13,30 @@ export default function Player() {
   console.log("URL:", url);
   console.log("Name:", name);
 
+  const sendDataToServer = async (data: any) => {
+    try {
+      const response = await axios.post('http://localhost:5000/model', data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error sending data to server:', error);
+    }
+  };
+
+  const data = {
+    title: 'Example Title',
+    description: 'Example Description',
+    category: 'Example Category',
+  };
+
   return (
     <div>
       <h1>{url}</h1>
       <ReactPlayer url={url as string} />
+      <Button onClick={() => sendDataToServer(data)}>Here</Button>
     </div>
   );
 }
