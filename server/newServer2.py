2c0e4afd734e270a8e202561a79590b1b740ae87
SagarNRao <sagarnrao154@gmail.com>
2025-06-09 14:55:54 +0530

    requirements

--- server/newServer2.py
+++ server/newServer2.py
@@ -143,4 +143,4 @@ def get_input_data():
         return jsonify({"message": "No data available"}), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
